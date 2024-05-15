dc697741a4f77bc7c2defbfdff7ca1f749277c07
Nicolas <n.abuin2005@gmail.com>
2024-05-15 20:34:34 +0200

    Fin de la parte 3

--- Parte 3/js/index.js
+++ Parte 3/js/index.js
@@ -9,8 +9,54 @@ const fetchData = async () => {
 
     console.log('datos de la api :', data)
 
+    printData(data)
+
 }
+
+const printData = (datoPokemon) => {
+
+    const listPokemon = document.getElementById('poke_list')
+    //const listPokemon = document.querySelector('#poke_List') Nos valdria cualquier caso de los dos
+    console.log('elemento #poke_list', listPokemon)
+
+    const cardElement = document.createElement('div')
+
+    cardElement.classList.add('card');
+    
+    cardElement.innerHTML = 
+       `<div class="card-header"></div>
+            <div class="card-body">
+            <img
+                src="./images/pokeball.png"
+                alt="imagen de pokemon"
+                class="card-body-img"
+            />
+            <h1 class="card-body-title clickable">${datoPokemon.name}</h1>
+            <p class="card-body-text">id</p>
+            </div>
+            <div class="card-footer">
+            <div class="card-footer-social">
+                <h3 id="card-footer-type">type</h3>
+                <p>Type</p>
+            </div>
+            <div class="card-footer-social">
+                <h3 id="card-footer-height">height</h3>
+                <p>Height</p>
+            </div>
+            <div class="card-footer-social">
+                <h3 id="card-footer-weight">weight</h3>
+                <p>Weight</p>
+            </div>
+        </div>`
+
+    listPokemon.appendChild(cardElement)
+
+    console.log('elemento card', cardElement)
+
+}
+
 
 //Llamamos a la funcion que hace el fetch
 fetchData()
+
 
