351b7b026f91b00f644d2ee97931b855e5848919
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-14 17:31:59 +0200

    Parte 8 y poco del 9

---
+++ Parte 9/js/index.js
@@ -0,0 +1,57 @@
+let data = [
+    "ABC",
+    "ABC's",
+    "ABCs",
+    "ABD",
+    "ABDs",
+    "ABM",
+    "ABM's",
+    "ABMs",
+    "ABS",
+    "AC",
+    "AC's",
+    "ACLU",
+    "ACT",
+    "ACTH",
+    "ACTH's",
+    "AD",
+    "AD's",
+    "ADC",
+    "ADD",
+    "ADP",
+    "ADP's",
+    "ADR",
+    "ADRs",
+    "AEC",
+    "AEC's",
+    "AF",
+    "AFAIK",
+    "AFAIKs",
+    "AFB",
+    "AFC",
+    "AFDC",
+    "AFN",
+    "AFT",
+    "AGC",
+    "AHQ",
+    "AI",
+    "AI's",
+    "AIDS",
+    "AIDS's",
+    "AIDSes",
+    "AIs",
+    "AK",
+    "AL",
+    "ALGOL",
+    "ALGOL's",
+    "ALU",
+    "AM",
+    "AM's"
+]
+
+
+function checkAnagrams(){
+
+let dataIsArray
+
+}
